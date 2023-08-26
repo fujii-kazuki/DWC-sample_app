51fa9beef51b60c4df0efcf63b28fda7b6a195d5
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-08-26 13:31:14 +0000

    [Update] resourcesメソッドを用いてのルーティングの書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'lists/new'
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+
+  # get 'lists/new'
+  # post 'lists' => 'lists#create'
+  # get 'lists' => 'lists#index'
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+
+  # 上記7つのルーティングを自動生成するメソッド
+  resources :lists
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
